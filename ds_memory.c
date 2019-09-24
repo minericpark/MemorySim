208ef3b57e64078cf9cde2521fd9047ef4451e4f
Eric Park <epark03@uoguelph.ca>
2019-09-23 23:07:07 -0400

    Completed ds_malloc, ds_free and working on ds_finish

--- ds_memory.c
+++ ds_memory.c
@@ -20,8 +20,8 @@ int main () {
 
 	#ifdef DEBUG
 		int i = 0;
-		char buffer[ds_file.block->length];
 		int size = 1234;
+		
 		
 		printf ("Debugging is turned on\n");
 		printf ("sizeof(struct ds_counts_struct) = %ld\n", sizeof(struct ds_counts_struct));
@@ -29,14 +29,27 @@ int main () {
 		printf ("sizeof(struct ds_file_struct) = %ld\n", sizeof(struct ds_file_struct));
 	
 		ds_create ("test", size); /*Testing*/
-		printf ("%s\n", ds_file.fp); /*Testing*/
-		ds_init ("test"); /*Testing*/
-
-		printf ("Block Num	Start	Lenght	Alloced\n");
+		
+		
+		printf ("%s\n", ds_file.fp); 
+		ds_init ("test");
+		
+		printf ("Searching for block of size 1234\n");
+		printf ("Return value is %ld\n", ds_malloc (10));
+		printf ("Searching for block of large size\n");
+		printf ("Return value is %ld\n", ds_malloc(100000));
+		
+		ds_free(0);
+		
+		printf ("Block Num	Start	Length	Alloced\n");
 		for (i = 0; i < MAX_BLOCKS; i++) {
 			printf ("%d		%ld	%ld 	%d\n", i, ds_file.block[i].start, ds_file.block[i].length, ds_file.block[i].alloced);
 		}
-		printf ("Read = %d, Write = %d", ds_counts.reads, ds_counts.writes);
+		printf ("Read = %d, Write = %d\n", ds_counts.reads, ds_counts.writes);
+		
+		ds_finish();
+		
+		
 	#endif
 	
     return (0); /*Program closes*/
@@ -81,7 +94,9 @@ int ds_init (char *filename) {
 	
 	if ((ds_file.fp = fopen (filename, "rb+")) == NULL) {
 		printf ("Error: file could not be opened\n");
+		return (1);
 	}
+	/*Error check when file reads in*/
 	fread (&ds_file, sizeof(ds_file), 0, ds_file.fp);
 	ds_counts.reads = 0;
 	ds_counts.writes = 0;
@@ -91,11 +106,41 @@ int ds_init (char *filename) {
 
 long ds_malloc (long amount) {
 	
-	return (0);
+	int i;
+	long tempStart;
+	long tempLength;
+	int secondBlockFound = 0;
+	
+	for (i = 0; i < MAX_BLOCKS; i++) {
+		if (ds_file.block[i].length >= amount && ds_file.block[i].alloced == 0) {
+			tempStart = ds_file.block[i].start;
+			tempLength = ds_file.block[i].length;
+			ds_file.block[i].length = amount;
+			ds_file.block[i].alloced = 1;
+			for (i = 0; i < MAX_BLOCKS; i++) {
+				if (ds_file.block[i].length == 0 && secondBlockFound == 0) {
+					secondBlockFound = 1;
+					ds_file.block[i].start = tempStart + amount;
+					ds_file.block[i].length = tempLength - amount;
+					ds_file.block[i].alloced = 0;
+				}
+			}
+			return ds_file.block[i].start;
+		}			
+	}	
+	return (-1);
 	
 }
 
 void ds_free (long start) {
+	
+	int i;
+	
+	for (i = 0; i < MAX_BLOCKS; i++) {
+		if (ds_file.block[i].start == start) {
+			ds_file.block[i].alloced = 0;
+		}
+	}
 	
 }
 
@@ -116,6 +161,15 @@ long ds_write (long start, void *ptr, long bytes) {
 
 int ds_finish () {
 	
+	int i;
+	
+	fseek (ds_file.fp, 0, SEEK_SET);
+	/*Error check when file reads in*/
+	/*fwrite (&ds_file, sizeof(ds_file), 0, ds_file.fp);*/
+	for (i = 0; i < MAX_BLOCKS; i++) {
+		fwrite (&ds_file.block[i], sizeof (struct ds_blocks_struct), 1, ds_file.fp);
+	}
+	fclose (ds_file.fp);
 	return (0);
 	
 }
