7c2217f52ac38daafa113deec6b0abc0bd1e7cfa
Eric Park <epark03@uoguelph.ca>
2019-09-26 15:48:44 -0400

    Completed ds_finish_array + ds_init_array, modified makefile

--- ds_array.c
+++ ds_array.c
@@ -6,13 +6,36 @@
 /*#define DEBUG*/
 /*#define TEST*/
 
-long element;
+long elements;
 
-int main() {
+int main(int argc, char **argv) {
+	
 	
 	ds_create ("array.bin", 2048);
 	ds_create_array();
 	show_array();
+	
+
+	/*
+	int value;
+	long index;
+
+	if(argc!=3)
+	{ 
+		fprintf( stderr,"Usage:  %svalue index\n", argv[0] );
+		return-1;
+	}
+
+	value = atoi( argv[1] );
+	index = atol( argv[2] );
+
+	ds_init_array();
+
+	ds_insert( value, index );
+
+	ds_finish_array();
+	*/
+
 	return 0;
 	
 }
@@ -20,19 +43,19 @@ int main() {
 int ds_create_array() {
 	
 	if (ds_init("array.bin") == -1) {
-		printf ("Error: ds_init failed");
+		printf ("Error: ds_init failed\n");
 		return -1;
 	}
 	if (ds_malloc(sizeof(long)) == -1) {
-		printf ("Error: initial ds_malloc failed");
+		printf ("Error: initial ds_malloc failed\n");
 		return -1;
 	}
 	if (ds_malloc(sizeof(int)*MAX_ELEMENTS) == - 1) {
-		printf ("Error: ds_malloc for array failed");
+		printf ("Error: ds_malloc for array failed\n");
 		return -1;
 	}
 	if (ds_finish() != 1) {
-		printf ("Error: ds_finish failed");
+		printf ("Error: ds_finish failed\n");
 		return -1;
 	}
 	return 0;
@@ -42,13 +65,20 @@ int ds_create_array() {
 /*Test function*/
 void show_array() {
 	
+	ds_init_array();
 	ds_test_init();
-	printf ("elements = %ld", element);
+	printf ("elements = %ld", elements);
+	ds_finish_array();
 	
 }
 
 int ds_init_array() {
 	
+	if (ds_init ("array.bin") != 0) {
+		printf ("ds_init failed\n");
+		return -1;
+	}
+	ds_read (&elements, 0, sizeof(long));
 	return 0;
 	
 }
@@ -60,6 +90,30 @@ int ds_replace (int value, long index) {
 
 int ds_insert (int value, long index) {
 	
+	int oldValue;
+	int newValue = value;
+	int location;
+	int inserted = 0;
+	int i;
+	
+	location = index * sizeof(int) + sizeof(elements);
+	
+	for (i = index; i < index + elements; i++) {
+		if (index == location && inserted == 0) {
+			ds_read (&oldValue, index, sizeof(int));
+			ds_write (index, &newValue, sizeof(int)); 
+			inserted = 1;
+		}
+		else if (inserted == 1) {
+			newValue = oldValue;
+			ds_read (&oldValue, index, sizeof(int));
+			ds_write (index, &newValue, sizeof(int));
+		}
+	}
+	if (inserted == 0) {
+		return -1;
+	}
+	elements++;
 	return 0;
 }
 
@@ -85,5 +139,10 @@ int ds_read_elements (char *filename) {
 
 int ds_finish_array() {
 	
+	ds_write (0, &elements, sizeof(long)); 
+	if (ds_finish() == 0) {
+		printf ("ds_finish failed\n");
+		return -1;
+	}
 	return 0;
 }
